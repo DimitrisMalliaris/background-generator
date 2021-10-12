de76e6b5baa9bd160d10d980a7bd897db7e7c612
Dimy993 <73944749+Dimy993@users.noreply.github.com>
2021-10-12 12:48:46 +0300

    adding starting project

---
+++ script.js
@@ -0,0 +1,74 @@
+var css = document.querySelector(".css");
+var colors = document.querySelectorAll(".color");
+var degree = document.querySelector(".degree");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+var container = document.querySelector(".colors");
+var random = document.querySelector(".random");
+var numOfColors = 2;
+
+
+colors[0].addEventListener("input", setGradient)
+colors[1].addEventListener("input", setGradient)
+degree.addEventListener("input", setGradient)
+
+button.addEventListener("click", function(){
+	numOfColors++;
+
+	var label = document.createElement("label");
+	label.setAttribute("for", "color" + numOfColors);
+	label.textContent = "Color " 
+	+ numOfColors 
+	+ ":";
+	if(numOfColors < 10){
+		label.textContent += " ";
+	}
+	container.appendChild(label);
+
+	var newColor =  document.createElement("input");
+	newColor.setAttribute("type", "color");
+	newColor.setAttribute("name", "color" + numOfColors);
+	newColor.setAttribute("class", "color");
+	newColor.addEventListener("input", setGradient);
+	container.appendChild(newColor);
+	colors = document.querySelectorAll(".color");
+
+	container.appendChild(document.createElement("br"));
+
+	setGradient();
+})
+
+random.addEventListener("click", function(){
+	colors.forEach(color => {
+		color.value = getRandomColor();
+	})
+	degree.value = Math.floor(Math.random() * Math.floor(365));
+	setGradient();
+})
+
+function getRandomColor(){
+	return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function setGradient(){
+	if( degree.value === undefined || degree.value === "") { 
+		degree.value = 0; 
+	}
+	var backgroundStyle = "linear-gradient("
+	+ degree.value 
+	+"deg, "  
+
+	colors.forEach(color => {
+		backgroundStyle += color.value;
+		backgroundStyle += ", "
+	})
+
+	backgroundStyle = backgroundStyle.slice(0,-2);
+
+	backgroundStyle += ") no-repeat";
+
+	body.style.background = backgroundStyle;
+	css.textContent = body.style.background + ";";
+}
+
+setGradient();
